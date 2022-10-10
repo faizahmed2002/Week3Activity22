0f23029b9145849f59065041bfb21fd797099b72
Faiz Ahmed <faizahmed2002@gmail.com>
2022-10-10 18:50:11 +0100

    Updated

--- Unsolved/script.js
+++ Unsolved/script.js
@@ -1,1 +1,21 @@
 //WRITE YOUR CODE BELOW
+
+//  It's done when the `customerOrder` object has three properties that store the drink's name, the number of sugars, and a Boolean indicating whether the order is ready. 
+
+//* It's done when the drink name and the number of sugars is logged to the console. 
+
+//* It's done when, if the order is ready, the message `"Ready for pick-up"` logs. 
+
+//* It's done when, if the order is not ready, the message `"Still in order queue"` logs. 
+
+//## 💡 Hints
+
+//How can we use dot notation or bracket notation to access an object's values? 
+
+const customerOrder = {
+    "Coffee", 
+    NumberOfSugars: 3 ,
+    isReady: false,
+}
+
+console.log ('the drink name is $(customerOrder.Drinkname')
